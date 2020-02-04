4868b789250bd0ea2cea1638e60b76de97e7c7ac
Shane Sopel <shanesopel@gmail.com>
2020-02-03 22:07:33 -0500

    added spark maxx encoder init function

--- 6120_2020/src/main/cpp/MotorSparkMax6120.cpp
+++ 6120_2020/src/main/cpp/MotorSparkMax6120.cpp
@@ -1,20 +1,44 @@
-//#include "Configs20.h"
+#include <frc/smartdashboard/smartdashboard.h>
 #include "MotorSparkMax6120.h"
 #include "Robot.h"
 
+	//This function will set our SPARK motors initalization
 	void  MotorSparkMax6120::SMAXXMotorInit()
 	{
-		/*
-		//set left motor mode and give outputs
-		_leftMotor1->Set(ControlMode::PercentOutput, leftDrive);
-		_leftMotor2->Set(ControlMode::PercentOutput, leftDrive);
 		
-		//set right motor mode and give outputs
-		_rightMotor1->Set(ControlMode::PercentOutput, rightDrive);
-		_rightMotor2->Set(ControlMode::PercentOutput, rightDrive);
-		*/
-		int x;
+		// We are settting our parameters from
+		// MotorSparkMaxx6120.h
+		this->kP         = 0.1;
+		this->kI         = 1e-4;
+		this->kD         = 1;
+		this->kIz        = 0;
+		this->kFF        = 0; 
+		this->kMinOutput =-1;
+		this->kMaxOutput = 1;  
 
+		// Setting motor to default factory Settings
+		motor1.RestoreFactoryDefaults();
+
+		// Setting feedback device aka setting what encoder to talk to.
+		pidController1.SetFeedbackDevice(encoder1);
+
+		// Set the PID with the parameters above.
+		pidController1.SetP(kP);
+		pidController1.SetI(kI);
+		pidController1.SetD(kD);
+		pidController1.SetIZone(kIz);
+		pidController1.SetFF(kFF);
+		pidController1.SetOutputRange(kMinOutput, kMaxOutput);
+
+		// display PID coefficients on SmartDashboard
+    	frc::SmartDashboard::PutNumber("P Gain", kP);
+    	frc::SmartDashboard::PutNumber("I Gain", kI);
+    	frc::SmartDashboard::PutNumber("D Gain", kD);
+    	frc::SmartDashboard::PutNumber("I Zone", kIz);
+    	frc::SmartDashboard::PutNumber("Feed Forward", kFF);
+    	frc::SmartDashboard::PutNumber("Max Output", kMaxOutput);
+    	frc::SmartDashboard::PutNumber("Min Output", kMinOutput);
+    	frc::SmartDashboard::PutNumber("Set Rotations", 0);
 	}  
 	
 void  MotorSparkMax6120::SMAXXIntakeControl()
--- 6120_2020/src/main/include/MotorSparkMax6120.h
+++ 6120_2020/src/main/include/MotorSparkMax6120.h
@@ -7,21 +7,30 @@
   //setting up motor
   rev::CANSparkMax motor1{0, rev::CANSparkMax::MotorType::kBrushed};
 
-  class MotorVictor6120
-  {
-   public:
+  // setting up motors can
+  rev::CANEncoder encoder1 = motor1.GetEncoder(rev::CANEncoder::EncoderType::kQuadrature, 4096);
 
-    void VicMotorInit(double leftDrive, double rightDrive);
-    void IntakeControl(double intakeOut, double intakeIn);
-    void ArmControl(double toggle, double armControl); 
-  };
+  //Use Pid functionality
+  rev::CANPIDController pidController1 = motor1.GetPIDController();
 
   class MotorSparkMax6120
   {
-    void SMAXXMotorInit();
+    
+    public:
+    void SMAXXMotorInit();     // this will setup parameters so we can get encoder data. we will need to read encoder data to set up conditions/parameters
     void SMAXXIntakeControl(); // previous year functions.
     void SMAXXArmControl();    // previous year functions.
                                // we will need to set up the functions we use this year and add encoder capability. 
+    private:
+    // PID coefficients
+    double kP; 
+    double kI;
+    double kD;
+    double kIz;
+    double kFF;
+    double kMaxOutput; 
+    double kMinOutput;
+
   };
 
 #endif
